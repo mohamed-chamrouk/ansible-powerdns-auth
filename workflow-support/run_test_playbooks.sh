c0fc1a1f70dc33ce4be794089376346c03697d5b
mohamed <mohamed-ch30@hotmail.com>
2025-09-09 13:56:51 +0200

    Update run_test_playbooks.sh

--- workflow-support/run_test_playbooks.sh
+++ workflow-support/run_test_playbooks.sh
@@ -4,5 +4,5 @@
 #ansible-playbook -i localhost, workflow-support/ansible-"${1}"/test-zone.yml
 #ansible-playbook -i localhost, workflow-support/ansible-"${1}"/test-zone-issue-136.yml
 #ansible-playbook -i localhost, workflow-support/ansible-"${1}"/test-zone-rrset.yml
-ansible-playbook -i localhost, workflow-support/ansible-2.19/test-rrset-record.yml -vvv
+ansible-playbook -i localhost, workflow-support/ansible-2.19/test-rrset-record.yml
 #ansible-playbook -i localhost, workflow-support/ansible-2.19/test-cryptokey.yml
