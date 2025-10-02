574b931185fc64f37cc4f7bee55f8cc1ebf1f973
mohamed <mohamed-ch30@hotmail.com>
2025-10-02 09:07:06 +0200

    debugging

--- workflow-support/run_test_playbooks.sh
+++ workflow-support/run_test_playbooks.sh
@@ -7,4 +7,4 @@ set -e # in order to fail on any playbook
 #ansible-playbook -i localhost, workflow-support/ansible-"${1}"/test-zone-issue-136.yml
 #ansible-playbook -i localhost, workflow-support/ansible-"${1}"/test-zone-rrset.yml
 #ansible-playbook -i localhost, workflow-support/ansible-"${1}"/test-rrset-record.yml
-ansible-playbook -i localhost, workflow-support/ansible-"${1}"/test-cryptokey.yml
+ansible-playbook -i localhost, workflow-support/ansible-"${1}"/test-cryptokey.yml -vvv
